341d967043f304c8ce7a39841b4b9022e203f92b
Thirumalesh <jakkathirumalesh342@gmail.com>
2025-02-25 21:30:15 +0530

    added validation in user.model

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 
+
 router.get("/register", (req, res) => {
   res.render("register");
 });

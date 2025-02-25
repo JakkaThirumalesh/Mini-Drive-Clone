37a88904b8fddb072f985b6a945b073c223b6939
Thirumalesh <jakkathirumalesh342@gmail.com>
2025-02-25 19:36:19 +0530

    added register functionality

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const userRoute = require("./routes/user.routes");
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/user", userRoute);
 
 app.listen(3000, () => {
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/text", (req, res) => {
-  res.send("user text route");
+
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+router.post("/register", (req, res) => {
+  console.log(req.body);
+  res.send("User Registered");
 });
 
 module.exports = router;

b58c37fe04a9381382c53e5530436b1eb44e14a3
Thirumalesh <jakkathirumalesh342@gmail.com>
2025-02-26 12:22:23 +0530

    added jwt and cookie in login

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const userModel = require("../models/user.model");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 router.get("/register", (req, res) => {
   res.render("register");
@@ -33,6 +34,58 @@ router.post(
     });
 
     res.json(newUser);
+  }
+);
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.post(
+  "/login",
+  body("username").trim().isLength({ min: 3 }),
+  body("password").trim().isLength({ min: 5 }),
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        error: errors.array(),
+        message: "Invalid data",
+      });
+    }
+    const { username, password } = req.body;
+
+    const user = await userModel.findOne({
+      username: username,
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "username or password is incorrect",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "username or password is incorrect",
+      });
+    }
+    /* jsonwebtoken */
+    const token = jwt.sign(
+      {
+        userId: user._id,
+        email: user.email,
+        username: user.username,
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.cookie("token", token);
+
+    res.send("Logged In");
   }
 );
 

4694d6c8d7be9af45dd16f1d4dcaa8b692d58855
Thirumalesh <jakkathirumalesh342@gmail.com>
2025-02-27 23:14:24 +0530

    added a home page and implemented a upload file components

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ dotenv.config();
 const connectToDB = require("./config/db");
 connectToDB();
 const cookieParser = require("cookie-parser");
-
 const app = express();
+const indexRoute = require("./routes/index.routes");
 
 app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/", indexRoute);
 app.use("/user", userRoute);
 
 app.listen(3000, () => {
